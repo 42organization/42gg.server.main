8ba1fa619c9e5c158754fe6d702497df52212ab5
taehyeon3 <117972735+taehyeon3@users.noreply.github.com>
2024-12-11 17:24:33 +0900

    Revert "feat: 개인일정 추가 API" (#1028)

---
+++ gg-calendar-api/src/main/java/gg/calendar/api/schedule/privateschedule/controller/request/PrivateScheduleReqDto.java
@@ -0,0 +1,4 @@
+package gg.calendar.api.schedule.privateschedule.controller.request;
+
+public class PrivateScheduleReqDto {
+}
--- gg-calendar-api/src/main/java/gg/calendar/api/schedule/privateschedule/service/PrivateScheduleService.java
+++ gg-calendar-api/src/main/java/gg/calendar/api/schedule/privateschedule/service/PrivateScheduleService.java
@@ -1,42 +1,22 @@
 package gg.calendar.api.schedule.privateschedule.service;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import gg.auth.UserDto;
-import gg.calendar.api.schedule.privateschedule.controller.request.PrivateScheduleCreateReqDto;
+import gg.calendar.api.schedule.privateschedule.controller.request.PrivateScheduleUpdateReqDto;
 import gg.data.calendar.PrivateSchedule;
-import gg.data.calendar.PublicSchedule;
-import gg.data.user.User;
+import gg.data.calendar.type.DetailClassification;
 import gg.repo.calendar.PrivateScheduleRepository;
 import gg.repo.calendar.PublicScheduleRepository;
-import gg.repo.user.UserRepository;
-import gg.utils.exception.user.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
-import gg.calendar.api.schedule.privateschedule.controller.request.PrivateScheduleUpdateReqDto;
 
 @Service
-@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class PrivateScheduleService {
 
 	private final PrivateScheduleRepository privateScheduleRepository;
 
 	private final PublicScheduleRepository publicScheduleRepository;
-  
-  private final UserRepository userRepository;
 
-  @Transactional
-	public void createPrivateSchedule(UserDto userDto, PrivateScheduleCreateReqDto privateScheduleCreateReqDto) {
-		PublicSchedule publicSchedule = PrivateScheduleCreateReqDto.of(userDto.getIntraId(),
-			privateScheduleCreateReqDto);
-		publicScheduleRepository.save(publicSchedule);
-		User user = userRepository.findById(userDto.getId()).orElseThrow(UserNotFoundException::new);
-		PrivateSchedule privateSchedule = PrivateSchedule.of(user, publicSchedule,
-			privateScheduleCreateReqDto.getColor());
-		privateScheduleRepository.save(privateSchedule);
-  }
-  
 	// Todo: 커스텀 에러 처리해야함
 	public PrivateSchedule updatePrivateSchedule(Long scheduleId, Long userId,
 		PrivateScheduleUpdateReqDto privateScheduleUpdateReqDto) {
