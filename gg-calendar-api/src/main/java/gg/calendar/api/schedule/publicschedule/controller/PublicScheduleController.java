cc29b08f56c7224fe0f3765cc4be1a2d5e48a384
WONIE <wonie@WONIEui-MacBookPro.local>
2024-12-14 21:51:13 +0900

    전체일정추가 api 생성 - req dto 생성 - controller 생성 - service 생성

--- gg-calendar-api/src/main/java/gg/calendar/api/schedule/publicschedule/controller/PublicScheduleController.java
+++ gg-calendar-api/src/main/java/gg/calendar/api/schedule/publicschedule/controller/PublicScheduleController.java
@@ -1,4 +1,35 @@
 package gg.calendar.api.schedule.publicschedule.controller;
 
+import javax.validation.Valid;
+
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import gg.auth.UserDto;
+import gg.auth.argumentresolver.Login;
+import gg.calendar.api.schedule.publicschedule.controller.request.PublicScheduleCreateReqDto;
+import gg.calendar.api.schedule.publicschedule.service.PublicScheduleService;
+import io.swagger.v3.oas.annotations.Parameter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/public")
 public class PublicScheduleController {
+	private final PublicScheduleService publicScheduleService;
+
+	@PostMapping("/create")
+	public ResponseEntity<Void> createPublicSchedule(
+		@Valid @ModelAttribute PublicScheduleCreateReqDto publicScheduleCreateReqDto,
+		@Login @Parameter(hidden = true) UserDto userDto) {
+		publicScheduleService.createPublicSchedule(userDto, publicScheduleCreateReqDto);
+		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
 }
--- gg-calendar-api/src/main/java/gg/calendar/api/schedule/publicschedule/service/PublicScheduleService.java
+++ gg-calendar-api/src/main/java/gg/calendar/api/schedule/publicschedule/service/PublicScheduleService.java
@@ -1,4 +1,32 @@
 package gg.calendar.api.schedule.publicschedule.service;
 
+
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import gg.auth.UserDto;
+import gg.calendar.api.schedule.publicschedule.controller.request.PublicScheduleCreateReqDto;
+import gg.data.calendar.PublicSchedule;
+import gg.data.user.User;
+import gg.repo.calendar.PublicScheduleRepository;
+import gg.repo.user.UserRepository;
+import gg.utils.exception.user.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class PublicScheduleService {
+	private final PublicScheduleRepository publicSchduleRepository;
+	private final UserRepository userRepository;
+
+	@Transactional
+	public void createPublicSchedule(UserDto userDto, PublicScheduleCreateReqDto publicScheduleCreateReqDto)
+	{
+		PublicSchedule publicSchedule = PublicScheduleCreateReqDto.of(userDto.getIntraId(),
+			publicScheduleCreateReqDto);
+		publicSchduleRepository.save(publicSchedule);
+	}
+
 }
