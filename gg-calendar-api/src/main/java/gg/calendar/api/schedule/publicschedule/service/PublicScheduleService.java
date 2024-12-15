d854fb2b03e6ffec8115d252e917bbe6e478bb1c
WONIE <wonie@WONIEui-MacBookPro.local>
2024-12-15 18:27:02 +0900

    기능추가

--- gg-calendar-api/src/main/java/gg/calendar/api/schedule/publicschedule/service/PublicScheduleService.java
+++ gg-calendar-api/src/main/java/gg/calendar/api/schedule/publicschedule/service/PublicScheduleService.java
@@ -5,11 +5,14 @@ package gg.calendar.api.schedule.publicschedule.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.nimbusds.oauth2.sdk.util.StringUtils;
+
 import gg.auth.UserDto;
 import gg.calendar.api.schedule.publicschedule.controller.request.PublicScheduleCreateReqDto;
 import gg.calendar.api.schedule.publicschedule.controller.request.PublicScheduleUpdateReqDto;
 import gg.calendar.api.schedule.publicschedule.controller.response.PublicScheduleUpdateResDto;
 import gg.data.calendar.PublicSchedule;
+import gg.data.calendar.type.DetailClassification;
 import gg.data.calendar.type.ScheduleStatus;
 import gg.data.user.User;
 import gg.repo.calendar.PublicScheduleRepository;
@@ -36,12 +39,33 @@ public class PublicScheduleService {
 	@Transactional
 	public PublicScheduleUpdateResDto updatePublicSchedule(Long scheduleId, Long userId, PublicScheduleUpdateReqDto publicScheduleUpdateReqDto){
 		PublicSchedule publicSchedule = publicSchduleRepository.findByUserIdAndSchduleId(userId, scheduleId).orElseThrow(()-> new IllegalArgumentException("해당 일정이 없습니다."));
+
+		validateScheduleUpdate(publicScheduleUpdateReqDto);
 		publicSchedule.update(publicScheduleUpdateReqDto.getClassification(), publicScheduleUpdateReqDto.getTags(),
 			publicScheduleUpdateReqDto.getTitle(), publicScheduleUpdateReqDto.getContent(),
 			publicScheduleUpdateReqDto.getLink(), publicScheduleUpdateReqDto.getStartTime(),
 			publicScheduleUpdateReqDto.getEndTime(), publicScheduleUpdateReqDto.isAlarm(),
 			publicScheduleUpdateReqDto.getColor());
 		return PublicScheduleUpdateResDto.from(publicSchedule);
+	}
+
+	private void validateScheduleUpdate(PublicScheduleUpdateReqDto publicScheduleUpdateReqDto)
+	{
+		if (publicScheduleUpdateReqDto.getStartTime().isAfter(publicScheduleUpdateReqDto.getEndTime()))
+		{
+			throw new IllegalArgumentException("시작 시간이 종료 시간보다 늦을 수 없습니다.");
+		}
+		if (publicScheduleUpdateReqDto.getEndTime().isBefore(publicScheduleUpdateReqDto.getStartTime()))
+		{
+			throw new IllegalArgumentException("종료시간이 시작시간보다 빠를 수 없습니다.");
+		}
+		if (publicScheduleUpdateReqDto.getEndTime().isEqual(publicScheduleUpdateReqDto.getStartTime()))
+		{
+			throw new IllegalArgumentException("시작 시간과 종료시간이 같을 수 없습니다.");
+		}
+		if (StringUtils.isBlank(publicScheduleUpdateReqDto.getTitle())) {
+			throw new IllegalArgumentException("일정 제목은 필수입니다.");
+		}
 	}
 
 	// TODO: 에러처리하기
