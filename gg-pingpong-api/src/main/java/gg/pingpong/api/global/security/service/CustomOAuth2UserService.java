2a3e5b67195ed21d0666f8a9c8681b98ff9275cd
seungsje <seungsje@student.42seoul.kr>
2024-12-23 15:37:13 +0900

    [bug] 42user일때만 Profile 만들게 수정

--- gg-pingpong-api/src/main/java/gg/pingpong/api/global/security/service/CustomOAuth2UserService.java
+++ gg-pingpong-api/src/main/java/gg/pingpong/api/global/security/service/CustomOAuth2UserService.java
@@ -1,26 +1,5 @@
 package gg.pingpong.api.global.security.service;
 
-import static gg.data.agenda.type.Coalition.*;
-
-import java.io.IOException;
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.security.authentication.InternalAuthenticationServiceException;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
-import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
-import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
 import gg.data.agenda.AgendaProfile;
 import gg.data.agenda.type.Coalition;
 import gg.data.pingpong.rank.Rank;
@@ -44,6 +23,26 @@ import gg.utils.RedisKeyManager;
 import gg.utils.exception.tier.TierNotFoundException;
 import gg.utils.external.ApiUtil;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+import static gg.data.agenda.type.Coalition.OTHER;
 
 @Service
 @RequiredArgsConstructor
@@ -93,14 +92,14 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 			savedUser = createUser(userInfo);
 			if (providerType.equals(ProviderType.FORTYTWO)) {
 				createUserRank(savedUser);
+				if (agendaProfileRepository.findByUserId(savedUser.getId()).isEmpty()) {
+					String token = userRequest.getAccessToken().getTokenValue();
+					createProfile(userInfo, savedUser, token);
+				}
 			}
 			if (userInfo.getImageUrl().startsWith("https://cdn.intra.42.fr/")) {
 				asyncNewUserImageUploader.upload(userInfo.getIntraId(), userInfo.getImageUrl());
 			}
-		}
-		if (agendaProfileRepository.findByUserId(savedUser.getId()).isEmpty()) {
-			String token = userRequest.getAccessToken().getTokenValue();
-			createProfile(userInfo, savedUser, token);
 		}
 
 		return UserPrincipal.create(savedUser, user.getAttributes());
@@ -158,7 +157,7 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
 		if (response != null && !response.isEmpty()) {
 			Map<String, Object> coalition = response.get(0);
-			String coalitionName = (String)coalition.get("name");
+			String coalitionName = (String) coalition.get("name");
 			return Coalition.valueOfCoalition(coalitionName);
 		} else {
 			return OTHER;
