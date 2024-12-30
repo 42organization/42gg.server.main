9351a15f1b084300d448141ebcc096968a140b47
taehyeon <kth0373@naver.com>
2024-12-30 16:27:37 +0900

    ✨ [Feature] 개인일정 수정 API Controller 수정 메서드 추가 #1057

--- gg-calendar-api/src/main/java/gg/calendar/api/user/schedule/privateschedule/controller/PrivateScheduleController.java
+++ gg-calendar-api/src/main/java/gg/calendar/api/user/schedule/privateschedule/controller/PrivateScheduleController.java
@@ -5,13 +5,17 @@ import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import gg.auth.UserDto;
 import gg.auth.argumentresolver.Login;
 import gg.calendar.api.user.schedule.privateschedule.controller.request.PrivateScheduleCreateReqDto;
+import gg.calendar.api.user.schedule.privateschedule.controller.request.PrivateScheduleUpdateReqDto;
+import gg.calendar.api.user.schedule.privateschedule.controller.response.PrivateScheduleUpdateResDto;
 import gg.calendar.api.user.schedule.privateschedule.service.PrivateScheduleService;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
@@ -27,5 +31,15 @@ public class PrivateScheduleController {
 		@Valid @RequestBody PrivateScheduleCreateReqDto privateScheduleCreateReqDto) {
 		privateScheduleService.createPrivateSchedule(userDto, privateScheduleCreateReqDto);
 		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<PrivateScheduleUpdateResDto> privateScheduleUpdate(
+		@Login @Parameter(hidden = true) UserDto userDto,
+		@Valid @RequestBody PrivateScheduleUpdateReqDto privateScheduleUpdateReqDto,
+		@RequestParam Long id) {
+		PrivateScheduleUpdateResDto privateScheduleUpdateResDto = privateScheduleService.updatePrivateSchedule(userDto,
+			privateScheduleUpdateReqDto, id);
+		return ResponseEntity.status(HttpStatus.OK).body(privateScheduleUpdateResDto);
 	}
 }
