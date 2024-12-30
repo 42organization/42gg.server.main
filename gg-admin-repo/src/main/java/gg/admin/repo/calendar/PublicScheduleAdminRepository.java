608f83a3eb31849e857dadc58a43bb1874f0174e
seyeon22222 <92151066+seyeon22222@users.noreply.github.com>
2024-12-30 13:45:21 +0900

    ✨ [Feature] Admin 공유일정 조회 API #1063 (#1084)

--- gg-admin-repo/src/main/java/gg/admin/repo/calendar/PublicScheduleAdminRepository.java
+++ gg-admin-repo/src/main/java/gg/admin/repo/calendar/PublicScheduleAdminRepository.java
@@ -2,13 +2,18 @@ package gg.admin.repo.calendar;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import gg.data.calendar.PublicSchedule;
+import gg.data.calendar.type.DetailClassification;
 
 @Repository
 public interface PublicScheduleAdminRepository extends JpaRepository<PublicSchedule, Long> {
 
 	List<PublicSchedule> findByAuthor(String author);
+
+	Page<PublicSchedule> findAllByClassification(DetailClassification detailClassification, Pageable pageable);
 }
