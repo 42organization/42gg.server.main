76f9c4b66de63c5804bb97d2b1770cd132ca59ab
seykim <kksy0917@gmail.com>
2025-01-02 16:00:44 +0900

    ✨ [Feature] Admin 개인일정 상세조회 API 추가

--- gg-admin-repo/src/main/java/gg/admin/repo/calendar/ScheduleGroupAdminRepository.java
+++ gg-admin-repo/src/main/java/gg/admin/repo/calendar/ScheduleGroupAdminRepository.java
@@ -1,4 +1,14 @@
 package gg.admin.repo.calendar;
 
-public interface ScheduleGroupAdminRepository {
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import gg.data.calendar.ScheduleGroup;
+
+@Repository
+public interface ScheduleGroupAdminRepository extends JpaRepository<ScheduleGroup, Long> {
+
+	Optional<ScheduleGroup> findByScheduleGroupId(Long scheduleGroupId);
 }
--- gg-calendar-api/src/main/java/gg/calendar/api/admin/schedule/privateschedule/service/PrivateScheduleAdminService.java
+++ gg-calendar-api/src/main/java/gg/calendar/api/admin/schedule/privateschedule/service/PrivateScheduleAdminService.java
@@ -4,8 +4,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import gg.admin.repo.calendar.PrivateScheduleAdminRepository;
+import gg.admin.repo.calendar.ScheduleGroupAdminRepository;
 import gg.calendar.api.admin.schedule.privateschedule.controller.response.PrivateScheduleAdminDetailResDto;
 import gg.data.calendar.PrivateSchedule;
+import gg.data.calendar.ScheduleGroup;
 import gg.utils.exception.ErrorCode;
 import gg.utils.exception.custom.NotExistException;
 import lombok.RequiredArgsConstructor;
@@ -17,13 +19,14 @@ public class PrivateScheduleAdminService {
 
 	private final PrivateScheduleAdminRepository privateScheduleAdminRepository;
 
-	private final
+	private final ScheduleGroupAdminRepository scheduleGroupAdminRepository;
 
 	public PrivateScheduleAdminDetailResDto detailPrivateSchedule(Long id) {
-
 		PrivateSchedule privateSchedule = privateScheduleAdminRepository.findById(id)
 			.orElseThrow(() -> new NotExistException(ErrorCode.PRIVATE_SCHEDULE_NOT_FOUND));
-
-		return PrivateScheduleAdminDetailResDto.toDto(privateSchedule);
+		ScheduleGroup scheduleGroup = scheduleGroupAdminRepository.findByScheduleGroupId(
+				privateSchedule.getGroupId())
+			.orElseThrow(() -> new NotExistException(ErrorCode.SCHEDULE_GROUP_NOT_FOUND));
+		return PrivateScheduleAdminDetailResDto.toDto(privateSchedule, scheduleGroup);
 	}
 }
