e1f2b789d450c5faf225811378dbd89384273e4d
taehyeon <kth0373@naver.com>
2025-01-07 13:58:51 +0900

    ✨ [Feature] 개인일정 기간 조회 API DTO 추가 #1061

---
+++ gg-calendar-api/src/main/java/gg/calendar/api/user/schedule/privateschedule/controller/response/PrivateSchedulePeriodResDto.java
@@ -0,0 +1,90 @@
+package gg.calendar.api.user.schedule.privateschedule.controller.response;
+
+import java.time.LocalDateTime;
+
+import gg.data.calendar.PrivateSchedule;
+import gg.data.calendar.ScheduleGroup;
+import gg.data.calendar.type.DetailClassification;
+import gg.data.calendar.type.EventTag;
+import gg.data.calendar.type.JobTag;
+import gg.data.calendar.type.ScheduleStatus;
+import gg.data.calendar.type.TechTag;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class PrivateSchedulePeriodResDto {
+	private Long id;
+
+	private DetailClassification classification;
+
+	private EventTag eventTag;
+
+	private JobTag jobTag;
+
+	private TechTag techTag;
+
+	private String author;
+
+	private String title;
+
+	private String content;
+
+	private String link;
+
+	private ScheduleStatus status;
+
+	private LocalDateTime startTime;
+
+	private LocalDateTime endTime;
+
+	private boolean alarm;
+
+	private String groupTitle;
+
+	private String groupColor;
+
+	@Builder
+	private PrivateSchedulePeriodResDto(Long id, DetailClassification classification, EventTag eventTag, JobTag jobTag,
+		TechTag techTag, String author, String title, String content, String link, ScheduleStatus status,
+		LocalDateTime startTime, LocalDateTime endTime, boolean alarm, String groupTitle, String groupColor) {
+		this.id = id;
+		this.classification = classification;
+		this.eventTag = eventTag;
+		this.jobTag = jobTag;
+		this.techTag = techTag;
+		this.author = author;
+		this.title = title;
+		this.content = content;
+		this.link = link;
+		this.status = status;
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.alarm = alarm;
+		this.groupTitle = groupTitle;
+		this.groupColor = groupColor;
+	}
+
+	public static PrivateSchedulePeriodResDto toDto(PrivateSchedule privateSchedule, ScheduleGroup scheduleGroup) {
+		return PrivateSchedulePeriodResDto.builder()
+			.id(privateSchedule.getId())
+			.classification(privateSchedule.getPublicSchedule().getClassification())
+			.eventTag(privateSchedule.getPublicSchedule().getEventTag())
+			.jobTag(privateSchedule.getPublicSchedule().getJobTag())
+			.techTag(privateSchedule.getPublicSchedule().getTechTag())
+			.author(privateSchedule.getPublicSchedule().getAuthor())
+			.title(privateSchedule.getPublicSchedule().getTitle())
+			.content(privateSchedule.getPublicSchedule().getContent())
+			.link(privateSchedule.getPublicSchedule().getLink())
+			.status(privateSchedule.getStatus())
+			.startTime(privateSchedule.getPublicSchedule().getStartTime())
+			.endTime(privateSchedule.getPublicSchedule().getEndTime())
+			.alarm(privateSchedule.isAlarm())
+			.groupTitle(scheduleGroup.getTitle())
+			.groupColor(scheduleGroup.getBackgroundColor())
+			.build();
+	}
+}
---
+++ gg-utils/src/main/java/gg/utils/dto/ListResponseDto.java
@@ -0,0 +1,25 @@
+package gg.utils.dto;
+
+import java.util.List;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ListResponseDto<T> {
+	private List<T> content;
+
+	@Builder
+	private ListResponseDto(List<T> content) {
+		this.content = content;
+	}
+
+	public static <T> ListResponseDto<T> toDto(List<T> content) {
+		return ListResponseDto.<T>builder()
+			.content(content)
+			.build();
+	}
+}
