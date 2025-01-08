cba4646a71dcf56a25b67d93697e9a3e61597034
taehyeon <kth0373@naver.com>
2025-01-08 17:31:54 +0900

    ✨ [Feature] 개인일정 커스터 마이징 목록 조회 API service 로직 추가 #1071

--- gg-calendar-api/src/main/java/gg/calendar/api/user/custom/service/CalendarCustomService.java
+++ gg-calendar-api/src/main/java/gg/calendar/api/user/custom/service/CalendarCustomService.java
@@ -1,5 +1,8 @@
 package gg.calendar.api.user.custom.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -7,6 +10,7 @@ import gg.auth.UserDto;
 import gg.calendar.api.user.custom.controller.request.CalendarCustomCreateReqDto;
 import gg.calendar.api.user.custom.controller.request.CalendarCustomUpdateReqDto;
 import gg.calendar.api.user.custom.controller.response.CalendarCustomUpdateResDto;
+import gg.calendar.api.user.custom.controller.response.CalendarCustomViewResDto;
 import gg.data.calendar.ScheduleGroup;
 import gg.data.user.User;
 import gg.repo.calendar.ScheduleGroupRepository;
@@ -36,5 +40,12 @@ public class CalendarCustomService {
 			.orElseThrow(() -> new NotExistException(ErrorCode.SCHEDULE_GROUP_NOT_FOUND));
 		scheduleGroup.update(calendarCustomUpdateReqDto.getTitle(), calendarCustomUpdateReqDto.getBackgroundColor());
 		return CalendarCustomUpdateResDto.toDto(scheduleGroup);
+	}
+
+	public List<CalendarCustomViewResDto> getScheduleGroupView(UserDto userDto) {
+		return scheduleGroupRepository.findByUserId(userDto.getId())
+			.stream()
+			.map(CalendarCustomViewResDto::toDto)
+			.collect(Collectors.toList());
 	}
 }
