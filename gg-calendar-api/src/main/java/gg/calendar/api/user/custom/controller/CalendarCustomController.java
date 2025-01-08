48c5f5ddb6e82d9e79a4ce8898b26d102b016641
taehyeon <kth0373@naver.com>
2025-01-08 14:37:13 +0900

    ✨ [Feature] 개인일정 커스터 마이징 추가 API 테스트 추가 #1137

--- gg-calendar-api/src/main/java/gg/calendar/api/user/custom/controller/CalendarCustomController.java
+++ gg-calendar-api/src/main/java/gg/calendar/api/user/custom/controller/CalendarCustomController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +24,7 @@ public class CalendarCustomController {
 
 	@PostMapping
 	public ResponseEntity<Void> scheduleGroupCreate(@Login @Parameter(hidden = true) UserDto userDto,
-		@Valid CalendarCustomCreateReqDto calendarCustomCreateReqDto) {
+		@Valid @RequestBody CalendarCustomCreateReqDto calendarCustomCreateReqDto) {
 		calendarCustomService.createScheduleGroup(userDto, calendarCustomCreateReqDto);
 		return ResponseEntity.status(HttpStatus.CREATED).build();
 	}
