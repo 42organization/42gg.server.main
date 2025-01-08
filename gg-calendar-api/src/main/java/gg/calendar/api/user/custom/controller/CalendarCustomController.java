c97c1ef16d2f49beddf2717e5439c3a82669a6ee
taehyeon <kth0373@naver.com>
2025-01-08 17:34:53 +0900

    ✨ [Feature] 개인일정 커스터 마이징 목록 조회 API controller 매핑 추가 #1071

--- gg-calendar-api/src/main/java/gg/calendar/api/user/custom/controller/CalendarCustomController.java
+++ gg-calendar-api/src/main/java/gg/calendar/api/user/custom/controller/CalendarCustomController.java
@@ -1,9 +1,12 @@
 package gg.calendar.api.user.custom.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -16,7 +19,9 @@ import gg.auth.argumentresolver.Login;
 import gg.calendar.api.user.custom.controller.request.CalendarCustomCreateReqDto;
 import gg.calendar.api.user.custom.controller.request.CalendarCustomUpdateReqDto;
 import gg.calendar.api.user.custom.controller.response.CalendarCustomUpdateResDto;
+import gg.calendar.api.user.custom.controller.response.CalendarCustomViewResDto;
 import gg.calendar.api.user.custom.service.CalendarCustomService;
+import gg.utils.dto.ListResponseDto;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
 
@@ -40,5 +45,12 @@ public class CalendarCustomController {
 		CalendarCustomUpdateResDto calendarCustomUpdateResDto = calendarCustomService.updateScheduleGroup(userDto,
 			calendarCustomUpdateReqDto, id);
 		return ResponseEntity.status(HttpStatus.OK).body(calendarCustomUpdateResDto);
+	}
+
+	@GetMapping
+	public ResponseEntity<ListResponseDto<CalendarCustomViewResDto>> scheduleGroupViewGet(
+		@Login @Parameter(hidden = true) UserDto userDto) {
+		List<CalendarCustomViewResDto> response = calendarCustomService.getScheduleGroupView(userDto);
+		return ResponseEntity.status(HttpStatus.OK).body(ListResponseDto.toDto(response));
 	}
 }
