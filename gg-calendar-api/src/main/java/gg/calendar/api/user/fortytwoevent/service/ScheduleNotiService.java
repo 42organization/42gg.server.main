eebcd7295722e675f6f411f5a4f3217d2886cec9
wonie <wonies@github.com>
2025-01-19 17:46:38 +0900

    [FIX] Notice 메세지 변경

--- gg-calendar-api/src/main/java/gg/calendar/api/user/fortytwoevent/service/ScheduleNotiService.java
+++ gg-calendar-api/src/main/java/gg/calendar/api/user/fortytwoevent/service/ScheduleNotiService.java
@@ -22,10 +22,10 @@ public class ScheduleNotiService {
 	private final PublicScheduleRepository publicScheduleRepository;
 	private final PrivateScheduleRepository privateScheduleRepository;
 	private final MessageSender messageSender;
-	private static final String SCHEDULE_MESSAGE_D_DAY = "일정요정🧚으로부터 알림이 도착했습니다.\n"
-		+ "오늘의 일정을 확인해보세요!\n";
-	private static final String SCHEDULE_MESSAGE_BEFORE_D_DAY = "일정요정🧚으로부터 알림이 도착했습니다.\n"
-		+ "내일의 일정을 확인해보세요!\n";
+	private static final String SCHEDULE_MESSAGE_D_DAY = "📆일정요정🧚으로부터 알림이 도착했습니다.\n"
+		+ "[42ggCalendar]와 오늘의 일정을 확인해보세요!\n";
+	private static final String SCHEDULE_MESSAGE_BEFORE_D_DAY = "📅일정요정🧚으로부터 알림이 도착했습니다.\n"
+		+ "[42ggCalendar]와 내일의 일정을 확인해보세요!\n";
 
 	@Transactional
 	public void sendScheduleNotifications() {
@@ -37,17 +37,13 @@ public class ScheduleNotiService {
 
 		List<PrivateSchedule> alarmSchedule = privateScheduleRepository.findSchedulesWithAlarmForBothDays(startOfDay,
 			endOfDay, startDday, endDday, ScheduleStatus.ACTIVATE);
-		log.info("startofday: {}", startOfDay);
-		log.info("endtofday: {}", endOfDay);
-		log.info("startDday: {}", startDday);
-		log.info("endDday: {}", endDday);
 		for (PrivateSchedule schedule : alarmSchedule) {
-			log.info("schedule, getID: {}", schedule.getId());
 			String message = schedule.getPublicSchedule().getEndTime()
 				.isBefore(currentTime.plusDays(1))
 				? SCHEDULE_MESSAGE_D_DAY : SCHEDULE_MESSAGE_BEFORE_D_DAY;
 			messageSender.send(schedule.getUser().getIntraId(),
-				message + schedule.getPublicSchedule().getTitle());
+				message + "[" + schedule.getPublicSchedule().getTitle() + "] : "
+					+ schedule.getPublicSchedule().getLink() + "\n");
 		}
 	}
 }
